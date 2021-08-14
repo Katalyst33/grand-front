28a59009df1cd9b84f3aa9ae137a452db0eb05f4
Your Name <you@example.com>
2021-08-14 16:37:53 +0100

    login page refactor

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -31,7 +31,7 @@
                     Email address
                   </label>
                   <div class="mt-1">
-                    <input v-model="form.email"  type="email" autocomplete="email" required=""
+                    <input v-model="form.email"  type="email" autocomplete="email"
                             class="appearance-none block w-full px-3 py-3 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-teal-500 focus:border-teal-500 sm:text-sm"/>
                   </div>
                 </div>
