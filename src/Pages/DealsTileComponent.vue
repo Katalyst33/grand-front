7910e2164b9b4ac24e9332e7b24f6da91b79bbda
Your Name <you@example.com>
2021-08-29 19:18:01 +0100

    deals to destinations

--- src/Pages/DealsTileComponent.vue
+++ src/Pages/DealsTileComponent.vue
@@ -4,8 +4,6 @@
     :key="index"
     class="group flex flex-col rounded-lg shadow-lg overflow-hidden relative"
   >
-
-
     <div class="flex-shrink-0">
       <router-link
         :to="{ name: 'ViewDealPage', params: { dealId: destination.uuid } }"
