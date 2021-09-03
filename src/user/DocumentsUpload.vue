c3cce6044bc3113c8ad9766f941997c287045acd
Your Name <you@example.com>
2021-09-03 21:05:28 +0100

    upload documents

--- src/user/DocumentsUpload.vue
+++ src/user/DocumentsUpload.vue
@@ -1,28 +1,93 @@
 <script lang="ts" setup>
 import { $axios } from "../http.Service";
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import { useRoute } from "vue-router";
 import { singleDestinationStore } from "../store/destinationStore";
 import { profileStore } from "../store/profileStore";
 import DragAndDrop from "../components/DragAndDrop.vue";
+import { appState } from "../store/store";
 const route = useRoute();
+const appUrl = import.meta.env.VITE_API_URL;
 
 const code = computed(() => route.params.destinationId);
 const imageData = new FormData();
+const imageInput = ref<HTMLInputElement>();
+const url = ref("");
+const file = ref("");
+const labelType = ref(" i am label type");
 
-const appUrl = import.meta.env.VITE_API_URL;
+function changeImage() {
+  imageInput.value?.click();
+  console.log(imageInput.value);
+}
+
+function onFileChange(e) {
+  file.value = e.target.files[0];
+  url.value = URL.createObjectURL(file.value);
+}
 
 function uploadDocuments() {
+  imageData.append("image", file.value);
+  imageData.append("bodyInfo", labelType.value);
+  console.log(imageData, "appp");
   $axios
-    .patch(`/profile/upload/${code.value}/images`)
-    .then((r) => console.log(r))
-    .catch((e) => e);
+
+    .patch(`/profile/upload/${appState.user.reference}/image`, imageData)
+
+    .then((r) => {
+      console.log(r);
+    })
+    .catch((e) => console.log(e));
 }
 </script>
 
 <template>
-  <DragAndDrop v-if="false" />
   <h3>Document Uploader !!</h3>
+
+  <div class="flex items-center gap-x-10">
+    <div
+      ref="uploaderArea"
+      style="width: 200px; height: 200px"
+      class="bg-red-500 rounded-md relative"
+    >
+      <a @click.prevent="changeImage" href="#" class="mx-auto">
+        <img v-if="!url" :src="`${appUrl}/uploads/app-logo.png`" />
+        <img v-else :src="url" />
+        <div
+          class="
+            absolute
+            bottom-0
+            py-2
+            w-full
+            text-center text-white
+            bg-gray-700
+            rounded-b-md
+            opacity-50
+          "
+        >
+          Click to Upload
+        </div>
+      </a>
+      <input
+        hidden
+        ref="imageInput"
+        type="file"
+        accept="image/*"
+        formenctype="multipart/form-data"
+        @change.prevent="onFileChange"
+      />
+    </div>
+
+    <div>
+      <button
+        @click.prevent="uploadDocuments"
+        class="bg-gray-600 text-white p-2 rounded-md"
+        type=""
+      >
+        upload
+      </button>
+    </div>
+  </div>
 
   <form action="/multiple_upload" enctype="multipart/form-data" method="POST">
     <div>
