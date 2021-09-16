4c2f202fc36508b86bf4f093fc24c182ad2a504c
Your Name <you@example.com>
2021-09-16 19:50:23 +0100

    innstall slider

--- src/views/ServicePage.vue
+++ src/views/ServicePage.vue
@@ -1,23 +1,9 @@
 <template>
-  <div>
-
-    <h1 >Service Page</h1>
-
-    <div class="grid grid-cols-4 gap-10" >
-      <div  v-for="item in cards" >
-        <p :class="item.color" class="p-40">
-          {{ item.title }} {{item.color}}
-        </p>
-      </div>
-    </div>
-
-  </div>
+  <div class="text-3xl">About This service</div>
 </template>
 
 <script lang="ts">
-
-import {defineComponent} from 'vue'
-
+import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "ServicePage",
@@ -25,40 +11,33 @@ export default defineComponent({
     return {
       cards: [
         {
-          title: 'red',
-          color: 'bg-red-500 col-span-2 lg:col-span-3',
-
+          title: "red",
+          color: "bg-red-500 col-span-2 lg:col-span-3",
         },
         {
-          title: 'yellow',
-          color: 'bg-yellow-500',
+          title: "yellow",
+          color: "bg-yellow-500",
         },
         {
-          title: 'blue',
-          color: 'bg-blue-500',
+          title: "blue",
+          color: "bg-blue-500",
         },
         {
-          title: 'green',
-          color: 'bg-green-500',
+          title: "green",
+          color: "bg-green-500",
         },
         {
-          title: 'gray',
-          color: 'bg-gray-500 md:col-span-1 lg:col-span-2',
+          title: "gray",
+          color: "bg-gray-500 md:col-span-1 lg:col-span-2",
         },
-
-      ]
-    }
+      ],
+    };
   },
 
   setup() {
-
-    return {}
-  }
-
-
-})
+    return {};
+  },
+});
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
