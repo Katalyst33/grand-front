538a9c7daf23150b492e06006fcc4e5502cc3e68
Your Name <you@example.com>
2021-12-12 12:03:54 +0000

    admin view user detail(s)

--- src/admin/views/UserDetails.vue
+++ src/admin/views/UserDetails.vue
@@ -1,14 +1,84 @@
 <template>
-  <div>
-    <div v-if="isLoaded">
-      <h1>User Details</h1>
-      {{ user.email }}
+  <div v-if="isLoaded">
+    <div
+      class="flex justify-between items-center bg-white border rounded-md my-2 py-1 px-2"
+    >
+      <h4 class="text-lg">{{ user.email }}</h4>
+      <div class="flex items-center gap-4">
+        <Listbox as="div" v-model="selected">
+          <div class="mt-1 relative">
+            <ListboxButton
+              class="relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            >
+              <span class="flex items-center">
+                <i :class="selected.icon"></i>
+                <span class="ml-3 block truncate">{{ selected.name }}</span>
+              </span>
+              <span
+                class="ml-3 absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none"
+              >
+                <SelectorIcon
+                  class="h-5 w-5 text-gray-400"
+                  aria-hidden="true"
+                />
+              </span>
+            </ListboxButton>
+
+            <transition
+              leave-active-class="transition ease-in duration-100"
+              leave-from-class="opacity-100"
+              leave-to-class="opacity-0"
+            >
+              <ListboxOptions
+                class="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-56 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm"
+              >
+                <ListboxOption
+                  as="template"
+                  v-for="(role, index) in roles"
+                  :key="index"
+                  :value="role"
+                  v-slot="{ active, selected }"
+                >
+                  <li
+                    :class="[
+                      active ? 'text-white bg-indigo-600' : 'text-gray-900',
+                      'cursor-default select-none relative py-2 pl-3 pr-9',
+                    ]"
+                  >
+                    <div class="flex items-center">
+                      <i :class="role.icon"></i>
+                      <span
+                        :class="[
+                          selected ? 'font-semibold' : 'font-normal',
+                          'ml-3 block truncate',
+                        ]"
+                      >
+                        {{ role.name }}
+                      </span>
+                    </div>
+
+                    <span
+                      v-if="selected"
+                      :class="[
+                        active ? 'text-white' : 'text-indigo-600',
+                        'absolute inset-y-0 right-0 flex items-center pr-4',
+                      ]"
+                    >
+                      <CheckIcon class="h-5 w-5" aria-hidden="true" />
+                    </span>
+                  </li>
+                </ListboxOption>
+              </ListboxOptions>
+            </transition>
+          </div>
+        </Listbox>
+        <button class="bg-yellow-500 rounded-md px-2 py-1 text-white">
+          Update Role
+        </button>
+      </div>
     </div>
 
-    <section>gfhjghk</section>
-
     <XAllProfilesComponent :all-profiles="allProfiles" :is-loaded="isLoaded" />
-    <div></div>
   </div>
 </template>
 
@@ -17,7 +87,35 @@ import { useRoute } from "vue-router";
 import { onMounted } from "vue";
 import XAllProfilesComponent from "../components/XAllProfilesComponent.vue";
 import { getUserDetails } from "../../http/admin.Service";
+import { ref } from "vue";
+import {
+  Listbox,
+  ListboxButton,
+  ListboxLabel,
+  ListboxOption,
+  ListboxOptions,
+} from "@headlessui/vue";
+import { CheckIcon, SelectorIcon } from "@heroicons/vue/solid";
 
+const roles = [
+  {
+    id: 1,
+    name: "user",
+    icon: "fad fa-user",
+  },
+  {
+    id: 2,
+    name: "staff",
+    icon: "fas fa-user-tie",
+  },
+  {
+    id: 3,
+    name: "admin",
+    icon: "fad fa-user-cog text-red-500",
+  },
+];
+
+const selected = ref(roles[2]);
 const route = useRoute();
 
 const { fetch, isLoaded, user, allProfiles } = getUserDetails(
