90bdc827d1082b5f255888907efe5420065e22ed
katalyst 33 <sam@katalyst.tecg>
2022-01-14 13:37:39 +0000

    refactor service card

--- src/components/BannerSlider.vue
+++ src/components/BannerSlider.vue
@@ -6,78 +6,82 @@
       class="relative w-screen bg-gray-300 h-96 lg:h-[600px] w-full object-cover bg-cover placeholder-img"
       style="left: calc(-50vw + 50%)"
     ></div>
-    <div v-show="showImage">
-      <div
-        v-if="isLoaded"
-        class="relative w-screen"
-        style="left: calc(-50vw + 50%)"
-      >
-        <Splide :options="options">
-          <SplideSlide
-            v-for="(destination, index) in promotedDestinations"
-            :key="index"
-            :data-splide-interval="3000"
-            class="group flex flex-col shadow-lg overflow-hidden relative"
-          >
-            <div class="relative">
-              <div class="absolute inset-x-0 bottom-0 h-1/2 bg-gray-100"></div>
-              <div class="">
-                <div class="relative shadow-xl sm:overflow-hidden py-24">
-                  <div class="absolute inset-0">
-                    <div
-                      class="absolute inset-0 bg-gray-400 mix-blend-multiply"
-                    ></div>
+    <transition name="fade">
+      <div v-show="showImage">
+        <div
+          v-if="isLoaded"
+          class="relative w-screen"
+          style="left: calc(-50vw + 50%)"
+        >
+          <Splide :options="options">
+            <SplideSlide
+              v-for="(destination, index) in promotedDestinations"
+              :key="index"
+              :data-splide-interval="3000"
+              class="group flex flex-col shadow-lg overflow-hidden relative"
+            >
+              <div class="relative">
+                <div
+                  class="absolute inset-x-0 bottom-0 h-1/2 bg-gray-100"
+                ></div>
+                <div class="">
+                  <div class="relative shadow-xl sm:overflow-hidden py-24">
+                    <div class="absolute inset-0">
+                      <div
+                        class="absolute inset-0 bg-gray-400 mix-blend-multiply"
+                      ></div>
 
-                    <img
-                      @load="imageLoaded"
-                      v-if="destination.thumbnails[0]"
-                      class="h-full w-full object-cover"
-                      :src="destination.thumbnails[0].path"
-                    />
-                    <img
-                      v-else
-                      @load="imageLoaded"
-                      class="h-full w-full object-cover"
-                      src="/hero/destinations/no-destination.jpeg"
-                    />
-                  </div>
+                      <img
+                        @load="imageLoaded"
+                        v-if="destination.thumbnails[0]"
+                        class="h-full w-full object-cover"
+                        :src="destination.thumbnails[0].path"
+                      />
+                      <img
+                        v-else
+                        @load="imageLoaded"
+                        class="h-full w-full object-cover"
+                        src="/hero/destinations/no-destination.jpeg"
+                      />
+                    </div>
 
-                  <div
-                    class="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8"
-                  >
-                    <h1
-                      class="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl"
-                    >
-                      <span class="block text-white lg:text-7xl big-font">{{
-                        destination.title
-                      }}</span>
-                    </h1>
-                    <p
-                      class="mt-6 max-w-lg mx-auto text-center text-xl text-yellow-200 sm:max-w-3xl"
-                    >
-                      {{ destination.text }}
-                    </p>
                     <div
-                      class="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center"
+                      class="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8"
                     >
-                      <router-link
-                        :to="{
-                          name: 'ViewDestinationPage',
-                          params: { destinationId: destination.uuid },
-                        }"
-                        class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-yellow-700 bg-white hover:bg-yellow-50 sm:px-8"
+                      <h1
+                        class="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl"
                       >
-                        View Destination
-                      </router-link>
+                        <span class="block text-white lg:text-7xl big-font">{{
+                          destination.title
+                        }}</span>
+                      </h1>
+                      <p
+                        class="mt-6 max-w-lg mx-auto text-center text-xl text-yellow-200 sm:max-w-3xl"
+                      >
+                        {{ destination.text }}
+                      </p>
+                      <div
+                        class="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center"
+                      >
+                        <router-link
+                          :to="{
+                            name: 'ViewDestinationPage',
+                            params: { destinationId: destination.uuid },
+                          }"
+                          class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-yellow-700 bg-white hover:bg-yellow-50 sm:px-8"
+                        >
+                          View Destination
+                        </router-link>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </SplideSlide>
-        </Splide>
+            </SplideSlide>
+          </Splide>
+        </div>
       </div>
-    </div>
+    </transition>
   </div>
 </template>
 <script>
@@ -163,5 +167,13 @@ export default {
   background-size: 1000px 104px;
 
   padding: 0.5em 0.5em 0.5em;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
