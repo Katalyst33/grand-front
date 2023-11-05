8ced0302916e3bd146b97d4b9703e8e94ab5eb9f
katalyst33 <>
2023-11-05 12:59:46 +0000

    save work

---
+++ src/components/BusyButton.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+const props = defineProps({
+  isLoading: {
+    type: Boolean,
+    default: false,
+  },
+  isDisabled: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<template>
+  <button type="button" class="">
+    <span class="flex items-center justify-center">
+      <slot></slot>
+      <span class="pl-2" v-if="isLoading">
+        <i class="fad fa-spinner animate-spin"></i>
+      </span>
+    </span>
+  </button>
+</template>
---
+++ src/views/AboutPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="h-screen">
+      <h4 class="text-lg text-center p-10">
+        This is a page about grand Eagle travel and tours
+      </h4>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "AboutPage",
+
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
